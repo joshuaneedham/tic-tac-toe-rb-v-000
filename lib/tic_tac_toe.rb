8f434668500d0051f812d2a43afd65c6fbc4a69e
Joshua Needham <joshuabneedham@gmail.com>
2017-10-09 20:52:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  !position_taken?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
