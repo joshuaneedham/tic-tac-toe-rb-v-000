084b00cb2d3885f0329e08f2b29d556c3d75f24f
Joshua Needham <joshuabneedham@gmail.com>
2017-10-09 21:11:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,33 +19,47 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player)
-  board[index] = current_player
-end
-
-def position_taken?(board, index)
-  board[index] != " " && board[index] != ""
-end
-
+# code your #valid_move? method here
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  #Check for an empty board is equal to " "
+  if board[index] == " "
+    return false
+  end
+  if board[index] == ""
+    return false
+  end
+  if board[index] == nil
+    return false
+  end
+  if board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+# Input to Index and move method
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+# Add move to board
+def move(board, index, character = "X")
+  board[index] = character
+end
+
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
+  user_input = gets.strip
+  index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
   else
     turn(board)
   end
+  move(board, index)
 end
 
 def turn_count(board)
