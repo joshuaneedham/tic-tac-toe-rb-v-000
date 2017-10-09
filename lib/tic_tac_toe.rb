e09d6bd5f0a97cee3dc8ac362372b77ce404bfab
Joshua Needham <joshuabneedham@gmail.com>
2017-10-09 20:37:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,6 +9,8 @@ WIN_COMBINATIONS = [
   [1,4,7],
   [2,5,8]
 ]
+
+# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -17,24 +19,54 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, character = "X")
-  board[index] = character
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
-# code your #valid_move? method here
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
 def valid_move?(board, index)
-  if board[index] == "" || board[index] == " "
-    return true
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
   end
 end
 
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+def turn_count(board)
+  moves = 0
+  # Checking each position on the board for an entry
+  board.each do |move|
+    # If not empty increment moves by one
+    if move == "X" || move == "O"
+      moves += 1
+    end
+  end
+  #return moves with or without 'return'
+  moves
+end
+
+def current_player(board)
+  # if turn_count(board) % 2 == 0
+  #   return "X"
+  # else turn_count(board) % 2 != 0
+  #   return "O"
+  # end
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
 def won?(board)
@@ -73,26 +105,4 @@ def winner(board)
      win_index_1 = won?(board)[0]
      return board[win_index_1]
   end
-end
-
-def turn_count(board)
-  moves = 0
-  # Checking each position on the board for an entry
-  board.each do |move|
-    # If not empty increment moves by one
-    if move == "X" || move == "O"
-      moves += 1
-    end
-  end
-  #return moves with or without 'return'
-  moves
-end
-
-def current_player(board)
-  # if turn_count(board) % 2 == 0
-  #   return "X"
-  # else turn_count(board) % 2 != 0
-  #   return "O"
-  # end
-  turn_count(board) % 2 == 0 ? "X" : "O"
 end
